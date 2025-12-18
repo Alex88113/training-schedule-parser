b58cd5e2430f6a4b5ee3099eb9fc0814972ee8aa
Shurik <sanya.kucheryavvyh@bk.ru>
2025-12-18 11:02:19 +0300

    Реализован файл запуска бота

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
-import os
-import asyncio
-from loguru import logger
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
-from dotenv import load_dotenv
-import sys
-
-logger.remove()
-logger.add(sys.stderr, format='{time} {level} {message}', level='INFO')
-load_dotenv()
-
-bot = Bot(token=os.getenv('TOKEN_MY_BOT'))
-dp = Dispatcher()
-logger.info('Бот создан!')
-
-@dp.message(Command('start'))
-async def cmd_start(message: types.Message):
-    await message.answer('Hello Alex!')
-
+from commands_bot import *
+from loggers_module.logger_module import logger
 
 async def main():
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    logger.info('БОТ ЗАПУЩЕН!')
+if __name__ == '__main__':
+    logger.info('Бот успешно запущен!')
     asyncio.run(main())
