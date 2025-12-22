e898429a81fc9b380cc991517c34cd4fc35e7521
Shurik <sanya.kucheryavvyh@bk.ru>
2025-12-22 11:55:04 +0300

    Обновил файл __init__.py

--- schedule_parser/__init__.py
+++ schedule_parser/__init__.py
@@ -1,10 +1,11 @@
 from .auth import AuthClients
 from .parser import getting_schedule
+from .config_settings import *
 import asyncio
 
 
 async def main():
-    obj_class = AuthClients()
-    token_schedule:str = await obj_class. get_authorization()
+    obj_class = AuthClients(user_data=user_data)
+    token_schedule: str = await obj_class.get_authorization()
     result = await getting_schedule(token_schedule)
     return result
