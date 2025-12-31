caf0b9df03bbe8ac73a7fa183ab3965c31eab9d2
Alex88113 <sanya.kucheryavvkh@bk.ru>
2025-12-31 22:12:22 +0300

    Update config_settings.py

--- config_user_settings/config_settings.py
+++ config_user_settings/config_settings.py
@@ -13,13 +13,13 @@ class UserSettings(BaseModel):
     """
     Класс базовой валидации данных из .env
     """
-    username: str = Field(min_length=1)
+    top_username: str = Field(min_length=1)
     password: str = Field(min_length=1)
     application_key: str = Field(min_length=1)
     id_city: None = None
 
 user_data = {
-    'username': os.getenv('TOP_USERNAME'),
+    'top_username': os.getenv('TOP_USERNAME'),
     'password': os.getenv('PASSWORD'),
     'application_key': os.getenv('APPLICATION_KEY'),
     'id_city': None
