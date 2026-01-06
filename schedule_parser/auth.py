e608669d4882eb28fecb0bf490c51a5cfd65a8e2
Shurik <sanya.kucheryavvyh@bk.ru>
2026-01-07 01:34:42 +0300

    Реализован HttpClient: с настройкой соединения и параметрами при подключении к серверу.

--- schedule_parser/auth.py
+++ schedule_parser/auth.py
@@ -1,64 +1,21 @@
 import asyncio
-import os
-import sys
 
-from aiohttp import (
-    ClientConnectionError,
-    ServerConnectionError
-)
-from loguru import logger
 import aiohttp
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
+from abc import ABC, abstractmethod
 
-if parent_dir not in sys.path:
-    sys.path.insert(0, parent_dir)
 
-from config_user_settings.config_settings import *
+class HttpClient:
+    def __init___(self):
+        self.conn = aiohttp.TCPConnector(
+            limit=100,
+            limit_per_host=30,
+            ttl_dns_cache=450
+        )
 
-class AuthClients:
-    def __init__(self, user_data: dict):
-        self.user_data = UserSettings(**user_data)
-        self.base_url: str = 'https://msapi.top-academy.ru/api/v2/auth/login'
+        self.timeout_session = aiohttp.ClientTimeout(
+            total=30,
+            connect=20
+        )
 
-    async def get_authorization(self):
-        headers = {
-            'accept': 'application/json, text/plain, */*',  # ожидаемый результат в формате json
-            'accept-language': 'ru_RU, ru',
-            'authorization': 'Bearer null',
-            'content-type': 'application/json',  # говорим о том что отправляем на сервак
-            'origin': 'https://journal.top-academy.ru',  # обязательно для безопастности (Cros защита)
-            'referer': 'https://journal.top-academy.ru/',  # желательно
-            "accept-encoding": "gzip, deflate, br, zstd",
-            "accept-language": "ru_RU, ru",
-            'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "YaBrowser";v="25.10", "Yowser";v="2.5"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 YaBrowser/25.10.0.0 Safari/537.36'
-        }
-
-        user_data2 = {
-            'username': self.user_data.username,
-            'password': self.user_data.password,
-            'application_key': self.user_data.application_key,
-            'id_city': None
-        }
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.post(self.base_url, headers=headers, json=user_data2) as response:
-                    if response.status == 200:
-                        response_data = await response.json()
-                        return response_data['refresh_token']
-
-        except ServerConnectionError as error_connect:
-            logger.error('Ошибка со стороны сервера: {e}', e=error_connect)
-
-        except ClientConnectionError as error:
-            logger.error('Возникла ошибка при подключении к серверу: {e}', e=error)
-
-        except ConnectionRefusedError as error:
-            logger.error('Соединение отклонено: {e}', e=error)
+        self.session = aiohttp.ClientSession(connector=self.conn, timeout=self.timeout_session)
